c162d68bb1ec0e854dd74d232275b19155866623
liamzwbao <zhiv.bao@gmail.com>
2023-01-09 13:58:16 -0800

    Enable Feign REST Client for Service Invocation

--- src/main/java/edu/neu/microservices/currencyconversionservice/controller/CurrencyConversionController.java
+++ src/main/java/edu/neu/microservices/currencyconversionservice/controller/CurrencyConversionController.java
@@ -1,36 +1,31 @@
 package edu.neu.microservices.currencyconversionservice.controller;
 
 import edu.neu.microservices.currencyconversionservice.bean.CurrencyConversion;
+import edu.neu.microservices.currencyconversionservice.proxy.CurrencyExchangeProxy;
 import org.springframework.core.env.Environment;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/currency-conversion")
 public class CurrencyConversionController {
     private final Environment environment;
+    private final CurrencyExchangeProxy currencyExchangeProxy;
 
-    public CurrencyConversionController(Environment environment) {
+    public CurrencyConversionController(Environment environment, CurrencyExchangeProxy currencyExchangeProxy) {
         this.environment = environment;
+        this.currencyExchangeProxy = currencyExchangeProxy;
     }
 
     @GetMapping("/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion calculateCurrencyConversion(@PathVariable String from,
                                                           @PathVariable String to,
                                                           @PathVariable BigDecimal quantity) {
-        Map<String, String> uriVariables = Map.of("from", from, "to", to);
-        ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity(
-                "http://localhost:8000/currency-exchange/from/{from}/to/{to}",
-                CurrencyConversion.class,
-                uriVariables);
-        CurrencyConversion currencyConversion = responseEntity.getBody();
+        CurrencyConversion currencyConversion = currencyExchangeProxy.retrieveExchangeValue(from, to);
         if (currencyConversion == null) {
             throw new RuntimeException("Unable to find data from " + from + " to " + to);
         }
