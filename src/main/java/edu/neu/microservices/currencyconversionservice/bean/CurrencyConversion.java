380001d4d08f421ada061116d69edbad465b7dda
liamzwbao <zhiv.bao@gmail.com>
2023-01-09 11:33:47 -0800

    Create a hard coded CurrencyConversionController

---
+++ src/main/java/edu/neu/microservices/currencyconversionservice/bean/CurrencyConversion.java
@@ -0,0 +1,22 @@
+package edu.neu.microservices.currencyconversionservice.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class CurrencyConversion {
+    private Long id;
+    private String from;
+    private String to;
+    private BigDecimal quantity;
+    private BigDecimal conversionMultiple;
+    private BigDecimal totalAmount;
+    private String environment;
+}
