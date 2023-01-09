40fd242482d93d2bcc099a2949b19751603df17d
liamzwbao <zhiv.bao@gmail.com>
2023-01-09 11:19:55 -0800

    Create CurrencyExchangeRepository

---
+++ src/main/java/edu/neu/microservices/currencyexchangeservice/repository/CurrencyExchangeRepository.java
@@ -0,0 +1,8 @@
+package edu.neu.microservices.currencyexchangeservice.repository;
+
+import edu.neu.microservices.currencyexchangeservice.bean.CurrencyExchange;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CurrencyExchangeRepository extends JpaRepository<CurrencyExchange, Long> {
+    CurrencyExchange findByFromAndTo(String from, String to);
+}
